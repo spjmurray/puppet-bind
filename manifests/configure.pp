cd9a0b6d6a474f206efbc8736bde1f9db1263b1b
Simon Murray <spjmurray@yahoo.co.uk>
2015-02-15 12:43:33 +0000

    Make file user and group explicit
    Setting defaults led to them being propagated to concat::setup.  As a floating
    class it could be executed before the bind group was installed.

--- manifests/configure.pp
+++ manifests/configure.pp
@@ -8,14 +8,10 @@ class bind::configure {
   $zones = $bind::zones
   $rndc_key = $bind::rndc_key
 
-  File {
-    owner => 'root',
-    group => 'bind',
-  }
-
   # bind::zone implicitly requires this directory for anchoring
   file { '/var/lib/bind/zones':
     ensure => directory,
+    owner  => 'root',
     group  => 'bind',
     mode   => '0775',
   }
@@ -24,18 +20,24 @@ class bind::configure {
 
   file { '/etc/bind/named.conf':
     ensure  => file,
+    owner   => 'root',
+    group   => 'bind',
     mode    => '0644',
     content => template('bind/named.conf.erb'),
   }
 
   file { '/etc/bind/named.conf.controls':
     ensure  => file,
+    owner   => 'root',
+    group   => 'bind',
     mode    => '0666',
     content => template('bind/named.conf.controls.erb'),
   }
 
   file { '/etc/bind/named.conf.options':
     ensure  => file,
+    owner   => 'root',
+    group   => 'bind',
     mode    => '0644',
     content => template('bind/named.conf.options.erb'),
   }
@@ -43,11 +45,15 @@ class bind::configure {
   # bind::zone implicitly befores this file for anchoring
   concat { '/etc/bind/named.conf.local':
     ensure => present,
+    owner  => 'root',
+    group  => 'bind',
     mode   => '0644',
   }
 
   file { '/etc/bind/rndc.key':
     ensure  => file,
+    owner   => 'root',
+    group   => 'bind',
     mode    => '0640',
     content => template('bind/rndc.key.erb'),
   }
